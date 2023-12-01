552e59b5fbb02cae532c4b81a0739b0f6003d781
DmitriyZhariy <dimazar2004@gmail.com>
2023-12-01 21:11:15 +0300

    сделал добавление записей почти для всех таблиц, осталась только таблица bus_trip

--- website/databases/entry_database.py
+++ website/databases/entry_database.py
@@ -1,7 +1,7 @@
 import psycopg2
 from initalization_databases import get_db_connection
 
-def insert_busstop(id_bus_stop, number_people, bus_stop_name, coords):
+def insert_busstop(number_people, bus_stop_name, coords):
     conn = get_db_connection()
 
     cur = conn.cursor()
@@ -16,11 +16,78 @@ def insert_busstop(id_bus_stop, number_people, bus_stop_name, coords):
     cur.close()
     conn.close()
 
+def insert_driver(full_name, email, password):
+    conn = get_db_connection()
+
+    cur = conn.cursor()
+
+    cur.execute("""
+                INSERT INTO driver (full_name, email, password)
+                VALUES (%s, %s, %s)
+                """, (full_name, email, password))
+    
+    conn.commit()
+
+    cur.close()
+    conn.close()
+
+def insert_bus(bus_number):
+
+    conn = get_db_connection()
+
+    cur = conn.cursor()
+
+    cur.execute("""
+                INSERT INTO bus (bus_number)
+                VALUES (%s)
+                """, (bus_number,))
+    
+    conn.commit()
+
+    cur.close()
+    conn.close()
+
+def insert_route(id_route, id_bus_stop, serial_num_bustop):
+    
+    conn = get_db_connection()
+
+    cur = conn.cursor()
+
+    cur.execute("""
+                INSERT INTO route (id_route, id_bus_stop, serial_num_bustop)
+                VALUES (%s, %s, %s)
+                """, (id_route, id_bus_stop, serial_num_bustop))
+    
+    conn.commit()
+
+    cur.close()
+    conn.close()
+
+def insert_bus_trip(id_trip, id_route, id_bus_stop, 
+                    actual_arrival_time, id_driver, id_bus):
+       
+    conn = get_db_connection()
+
+    cur = conn.cursor()
+
+    cur.execute("""
+                INSERT INTO route (id_trip, id_route, id_bus_stop, 
+                    actual_arrival_time, id_driver, id_bus)
+                VALUES (%s, %s, %s, %s, %s, %s)
+                """, (id_trip, id_route, id_bus_stop,
+                      actual_arrival_time, actual_arrival_time,
+                      id_driver, id_bus))
+    
+    conn.commit()
+
+    cur.close()
+    conn.close()
+
 if __name__ == '__main__':
-    """
-    проверка что вводимые ключи уникальные
-    вообще я думаю, что id будет автоматически вводиться, 
-    руководитель не должен париться на этот счет,
-    хотя тут уже вопрос успеем ли мы
-    """
-    insert_busstop(10, 'vorona', '123.3214.7')
+    pass
+    # insert_busstop(1, 'zayats', '100.8.8')
+    # insert_driver('ivanov ivan ivanovich', 'ivanovich1980@mail.ru', 
+    #               'qwerty')
+    # insert_bus(42)
+    # insert_route(1, 3, 4)
+    # insert_bus_trip(1, 1, 5, 8:00, 1, 1)
--- website/databases/initalization_databases.py
+++ website/databases/initalization_databases.py
@@ -14,48 +14,59 @@ def initialize_databases():
 
     cur = conn.cursor()
 
-    # BUS STOP DB
-    cur.execute('DROP TABLE IF EXISTS bus_stop;')
-    cur.execute('CREATE TABLE bus_stop (id_bus_stop serial PRIMARY KEY,'
-                                    'number_people integer NOT NULL,'
-                                    'bus_stop_name text NOT NULL,'
-                                    'coords text NOT NULL);'
-                                    )
+    # # BUS STOP DB
+    # cur.execute('DROP TABLE IF EXISTS bus_stop;')
+    # cur.execute('CREATE TABLE bus_stop (id_bus_stop serial PRIMARY KEY,'
+    #                                 'number_people integer NOT NULL,'
+    #                                 'bus_stop_name text NOT NULL,'
+    #                                 'coords text NOT NULL UNIQUE);'
+    #                                 )
 
-    # BUS DB
-    cur.execute('DROP TABLE IF EXISTS bus;')
-    cur.execute('CREATE TABLE bus (id_bus serial PRIMARY KEY,'
-                                    'bus_number integer NOT NULL,'
-                                    'charge_level text NOT NULL,'
-                                    'number_passenger integer NOT NULL);'
-                                    )
+    # # BUS DB
+    # cur.execute('DROP TABLE IF EXISTS bus;')
+    # cur.execute('CREATE TABLE bus (id_bus serial PRIMARY KEY,'
+    #                                 'bus_number integer NOT NULL,'
+    #                                 'charge_level text NOT NULL DEFAULT 0,'
+    #                                 'number_passenger integer NOT NULL DEFAULT 0);'
+    #                                 )
 
-    # DRIVER DB
-    cur.execute('DROP TABLE IF EXISTS driver;')
-    cur.execute('CREATE TABLE driver (id_driver serial PRIMARY KEY,'
-                                    'full_name text NOT NULL,'
-                                    'email text NOT NULL,'
-                                    'password text NOT NULL,'
-                                    'nonstop text NOT NULL);'
-                                    )
+    # # DRIVER DB
+    # cur.execute('DROP TABLE IF EXISTS driver;')
+    # cur.execute('CREATE TABLE driver (id_driver serial PRIMARY KEY,'
+    #                                 'full_name text NOT NULL,'
+    #                                 'email text NOT NULL UNIQUE,'
+    #                                 'password text NOT NULL,'
+    #                                 'nonstop text DEFAULT 0);'
+    #                                 )
 
-    # ROUTE DB
-    cur.execute('DROP TABLE IF EXISTS route;')
-    cur.execute('CREATE TABLE route (id_route integer PRIMARY KEY,'
-                                    'id_bus_stop integer NOT NULL,'
-                                    'serial_num_bustop text NOT NULL);'
-                                    )
+    # # ROUTE DB
+    # cur.execute('DROP TABLE IF EXISTS route;')
+    # cur.execute("""CREATE TABLE route (
+    #             id_route integer,
+    #             id_bus_stop integer NOT NULL,
+    #             serial_num_bustop integer NOT NULL,
+    #             PRIMARY KEY (id_route, id_bus_stop),
+    #             FOREIGN KEY (id_bus_stop) REFERENCES bus_stop (id_bus_stop)
+    #             );
+    #             """)
 
     # BUS TRIP
     cur.execute('DROP TABLE IF EXISTS bus_trip;')
-    cur.execute('CREATE TABLE bus_trip (id_trip integer PRIMARY KEY,'
-                                    'id_route integer NOT NULL,'
-                                    'id_bus_stop integer NOT NULL,'
-                                    'actual_arrival_time time NOT NULL,'
-                                    'real_arrival_time time NOT NULL,'
-                                    'id_driver integer NOT NULL,'
-                                    'id_bus integer NOT NULL);'
-                                    )
+    cur.execute("""CREATE TABLE bus_trip (
+                id_trip integer NOT NULL,
+                id_route integer NOT NULL,
+                id_bus_stop integer NOT NULL,
+                actual_arrival_time time NOT NULL,
+                real_arrival_time time NOT NULL,
+                id_driver integer NOT NULL,
+                id_bus integer NOT NULL,
+                PRIMARY KEY (id_trip, id_bus_stop),
+                FOREIGN KEY (id_route, id_bus_stop) REFERENCES route (id_route, id_bus_stop),
+                FOREIGN KEY (id_bus_stop) REFERENCES bus_stop (id_bus_stop),
+                FOREIGN KEY (id_driver) REFERENCES driver (id_driver),
+                FOREIGN KEY (id_bus) REFERENCES bus (id_bus)
+                );
+                """)
     
     # GLOBAL CONDITIONS
     cur.execute('DROP TABLE IF EXISTS global_conditions;')
