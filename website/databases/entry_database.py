105e38c750e4ff88988be77bbbad386fb9a5e0af
dmitriyzhariy <dimazar2004@gmail.com>
2023-12-01 23:15:47 +0300

    теперь можно добавлять одну запись за раз

--- website/databases/entry_database.py
+++ website/databases/entry_database.py
@@ -65,29 +65,59 @@ def insert_route(id_route, id_bus_stop, serial_num_bustop):
 
 def insert_bus_trip(id_trip, id_route, id_bus_stop, 
                     actual_arrival_time, id_driver, id_bus):
-       
+
     conn = get_db_connection()
 
     cur = conn.cursor()
 
     cur.execute("""
-                INSERT INTO route (id_trip, id_route, id_bus_stop, 
-                    actual_arrival_time, id_driver, id_bus)
-                VALUES (%s, %s, %s, %s, %s, %s)
+                INSERT INTO bus_trip (id_trip, id_route, id_bus_stop, 
+                    actual_arrival_time, real_arrival_time, id_driver, id_bus)
+                VALUES (%s, %s, %s, %s, %s, %s, %s)
                 """, (id_trip, id_route, id_bus_stop,
-                      actual_arrival_time, actual_arrival_time,
-                      id_driver, id_bus))
+                        actual_arrival_time, actual_arrival_time,
+                        id_driver, id_bus))
     
     conn.commit()
 
     cur.close()
     conn.close()
 
+def add_new_route(list_bus_stop):
+    
+    conn = get_db_connection()
+    cur = conn.cursor()
+
+    cur.execute("""
+                SELECT MAX(id_route)
+                FROM route
+                """)
+    old_max_id = cur.fetchone()
+    print(old_max_id[0])
+
+
+    cur.close()
+    conn.close()
+
 if __name__ == '__main__':
-    pass
     # insert_busstop(1, 'zayats', '100.8.8')
-    # insert_driver('ivanov ivan ivanovich', 'ivanovich1980@mail.ru', 
-    #               'qwerty')
+    # insert_busstop(10, 'volk', '112.75.82')
+    # insert_busstop(6, 'tigr', '133.72.81')
+    # insert_busstop(7, 'sobaka', '150.2.1')
+
+    # insert_driver('ivanov ivan ivanovich', 
+    #             'ivanovich1980@mail.ru', 'qwerty')
+    
     # insert_bus(42)
-    # insert_route(1, 3, 4)
-    # insert_bus_trip(1, 1, 5, 8:00, 1, 1)
+    
+    # insert_route(1, 3, 1)
+    # insert_route(1, 1, 2)
+    # insert_route(1, 2, 3)
+    # insert_route(1, 4, 4)
+
+    # insert_bus_trip(1, 1, 3, '8:00', 1, 1)
+    # insert_bus_trip(1, 1, 1, '8:10', 1, 1)
+    # insert_bus_trip(1, 1, 2, '8:20', 1, 1)
+    # insert_bus_trip(1, 1, 4, '8:30', 1, 1)
+    add_new_route([3, 1, 2, 4])
+    pass
--- website/databases/initalization_databases.py
+++ website/databases/initalization_databases.py
@@ -14,41 +14,51 @@ def initialize_databases():
 
     cur = conn.cursor()
 
-    # # BUS STOP DB
-    # cur.execute('DROP TABLE IF EXISTS bus_stop;')
-    # cur.execute('CREATE TABLE bus_stop (id_bus_stop serial PRIMARY KEY,'
-    #                                 'number_people integer NOT NULL,'
-    #                                 'bus_stop_name text NOT NULL,'
-    #                                 'coords text NOT NULL UNIQUE);'
-    #                                 )
+    # BUS STOP DB
+    cur.execute('DROP TABLE IF EXISTS bus_stop;')
+    cur.execute("""
+                CREATE TABLE bus_stop (
+                id_bus_stop serial PRIMARY KEY,
+                number_people integer NOT NULL,
+                bus_stop_name text NOT NULL UNIQUE,
+                coords text NOT NULL UNIQUE
+                );
+                """)
 
-    # # BUS DB
-    # cur.execute('DROP TABLE IF EXISTS bus;')
-    # cur.execute('CREATE TABLE bus (id_bus serial PRIMARY KEY,'
-    #                                 'bus_number integer NOT NULL,'
-    #                                 'charge_level text NOT NULL DEFAULT 0,'
-    #                                 'number_passenger integer NOT NULL DEFAULT 0);'
-    #                                 )
+    # BUS DB
+    cur.execute('DROP TABLE IF EXISTS bus;')
+    cur.execute("""
+                CREATE TABLE bus (
+                id_bus serial PRIMARY KEY,
+                bus_number integer NOT NULL,
+                charge_level text NOT NULL DEFAULT 0,
+                number_passenger integer NOT NULL DEFAULT 0
+                );
+                """)
 
-    # # DRIVER DB
-    # cur.execute('DROP TABLE IF EXISTS driver;')
-    # cur.execute('CREATE TABLE driver (id_driver serial PRIMARY KEY,'
-    #                                 'full_name text NOT NULL,'
-    #                                 'email text NOT NULL UNIQUE,'
-    #                                 'password text NOT NULL,'
-    #                                 'nonstop text DEFAULT 0);'
-    #                                 )
+    # DRIVER DB
+    cur.execute('DROP TABLE IF EXISTS driver;')
+    cur.execute("""
+                CREATE TABLE driver (
+                id_driver serial PRIMARY KEY,
+                full_name text NOT NULL,
+                email text NOT NULL UNIQUE,
+                password text NOT NULL,
+                nonstop text DEFAULT 0
+                );
+                """)
 
-    # # ROUTE DB
-    # cur.execute('DROP TABLE IF EXISTS route;')
-    # cur.execute("""CREATE TABLE route (
-    #             id_route integer,
-    #             id_bus_stop integer NOT NULL,
-    #             serial_num_bustop integer NOT NULL,
-    #             PRIMARY KEY (id_route, id_bus_stop),
-    #             FOREIGN KEY (id_bus_stop) REFERENCES bus_stop (id_bus_stop)
-    #             );
-    #             """)
+    # ROUTE DB
+    cur.execute('DROP TABLE IF EXISTS route;')
+    cur.execute("""
+                CREATE TABLE route (
+                id_route integer,
+                id_bus_stop integer NOT NULL,
+                serial_num_bustop integer NOT NULL,
+                PRIMARY KEY (id_route, id_bus_stop),
+                FOREIGN KEY (id_bus_stop) REFERENCES bus_stop (id_bus_stop)
+                );
+                """)
 
     # BUS TRIP
     cur.execute('DROP TABLE IF EXISTS bus_trip;')
@@ -70,11 +80,13 @@ def initialize_databases():
     
     # GLOBAL CONDITIONS
     cur.execute('DROP TABLE IF EXISTS global_conditions;')
-    cur.execute('CREATE TABLE global_conditions (current_weather integer PRIMARY KEY,'
-                                    'date_now date DEFAULT current_date,'
-                                    'time_now time DEFAULT current_time,'
-                                    'fiesta boolean DEFAULT false);'
-                                    )
+    cur.execute("""
+                CREATE TABLE global_conditions (current_weather integer PRIMARY KEY,
+                date_now date DEFAULT current_date,
+                time_now time DEFAULT current_time,
+                fiesta boolean DEFAULT false
+                );
+                """)
 
     conn.commit()
 
